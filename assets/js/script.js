bd67c75cc9c0d177324b12dd0927acae2a7e4263
hokton-bd <skmhkt.p@gmail.com>
2021-01-02 20:10:04 +0900

    aa

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,5 +16,24 @@
 
     });
 
-
 }());
+
+$(function(){
+    $('.js-link').click(function(){
+      var adjust = -60;
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top + adjust;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+    });
+
+    $('#js-scrollTop').on('click', function() {
+
+        $('body,html').animate({scrollTop:0}, 400, 'swing');
+
+    });
+
+
+});
